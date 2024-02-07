190e5309aa2853004a9f75f7672fa37cc4b1b439
Makson91 <olesyaperelmuter@gmail.com>
2024-02-07 19:04:02 +0200

    lesson-02/06

---
+++ lesson-02/06.js
@@ -0,0 +1,14 @@
+let passportWithAddress = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "LA"
+    }
+};
+
+let passportWithAddressClone = structuredClone(passportWithAddress);
+passportWithAddressClone.address.city = "Bobryisk";
+
+console.log(passportWithAddress.address.city);
+console.log(passportWithAddressClone.address.city);
